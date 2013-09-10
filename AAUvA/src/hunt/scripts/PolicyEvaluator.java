56cb760e0bbda60516c9100a591e8ef827026b61
Lex Utama <a.utama@student-solutions.nl>
2013-09-10 12:40:42 +0200

    Iterative Policy Evaluator

---
+++ AAUvA/src/hunt/scripts/PolicyEvaluator.java
@@ -0,0 +1,77 @@
+package hunt.scripts;
+
+import hunt.controller.HuntController;
+import hunt.model.AbstractPredator;
+import hunt.model.HuntState;
+import hunt.model.board.Position;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class PolicyEvaluator {
+	
+	protected Map<HuntState, Double> values;
+	protected AbstractPredator policy;
+	
+	protected int iterations;
+	
+	public static final double THRESHOLD = 0.001;
+	public static final double GAMMA = 0.8;
+	
+	public PolicyEvaluator(AbstractPredator policy) {
+		values = new HashMap<HuntState, Double>();
+		this.policy = policy;
+	}
+
+	public void run() {
+		this.init();
+		
+		double difference = 1000;
+		this.iterations = 0;
+		while (difference > THRESHOLD) {
+			iterations++;
+			difference = 0;
+			
+			for (Entry<HuntState, Double> entry: values.entrySet()) {
+				HuntState oldState = entry.getKey();
+				Double oldValue = entry.getValue();
+				
+				double updatedValue = 0;
+				for (Position action : HuntController.getActions(oldState)) {
+					double actionProbability = policy.getActionProbability(oldState, action);
+					double actionValue = 0;
+					
+					for (HuntState newState : HuntController.getNextStates(oldState, action)) {
+						double transitionProbability = HuntController.getTransitionProbability(oldState, newState, action);
+						double transitionReward = HuntController.getReward(oldState, newState, action);
+						double newStateValue = values.get(newState);
+						
+						actionValue += transitionProbability * (transitionReward + GAMMA * newStateValue); 
+					}
+					
+					updatedValue += actionProbability * actionValue;
+				}
+				
+				this.values.put(oldState, updatedValue);
+				
+				difference = Math.max(difference, Math.abs(oldValue - updatedValue));
+			}
+		}
+	}
+	
+	public void init() {
+		for (HuntState state : HuntController.getAllStates()) {
+			values.put(state, new Double(0));
+		}
+	}
+	
+	public Map<HuntState, Double> getValues() {
+		return this.values;
+	}
+	
+	public int getIterations() {
+		return this.iterations;
+	}
+}
