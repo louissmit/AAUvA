59ac481a82e9dce5a63006b42defbc0c9b5c89e7
Lex Utama <a.utama@student-solutions.nl>
2013-09-24 17:57:07 +0200

    getAllStates

--- AAUvA/src/hunt/model/predator/LearningPredatorPolicy.java
+++ AAUvA/src/hunt/model/predator/LearningPredatorPolicy.java
@@ -1,8 +1,10 @@
 package hunt.model.predator;
 
 import hunt.model.HuntState;
+import hunt.model.RelativeState;
 import hunt.model.board.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +19,16 @@ public abstract class LearningPredatorPolicy extends PredatorPolicy {
 	
 	@Override
 	public List<HuntState> getAllStates() {
-		// TODO
-		return null;
+		List<HuntState> result = new ArrayList<HuntState>();
+		
+		// Predator position
+		for (int i = 0; i < Position.BWIDTH; i++) {
+			for (int j = 0; j < Position.BHEIGHT; j++) {
+				result.add(new RelativeState(new Position(i,j)));
+			}
+		}
+		
+		return result;
 	}
 	
 }
