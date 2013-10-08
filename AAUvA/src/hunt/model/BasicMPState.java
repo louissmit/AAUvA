422d1a7609b23103c9f0d6ddbfea93a38d9d646f
Hubert <huber_000@Hubert-dell>
2013-10-08 17:31:21 +0200

    BasicMpState all methods implemented

--- AAUvA/src/hunt/model/BasicMPState.java
+++ AAUvA/src/hunt/model/BasicMPState.java
@@ -4,20 +4,23 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import hunt.model.board.Position;
 
 public class BasicMPState extends MultiPredatorState {
 	
 	private Map<String, Position> predatorPositions;
+	private final double randomChanceForWait=0.2;
 	
 	public BasicMPState() {
 		predatorPositions = new HashMap<String, Position>();
 	}
 	
-	public void addPredator(String name, Position action) {
-		this.predatorPositions.put(name, action);
+	public void addPredator(String name, Position position) {
+		this.predatorPositions.put(name, position);
 	}
+	
 
 	@Override
 	public boolean isTerminal() {
@@ -36,26 +39,61 @@ public class BasicMPState extends MultiPredatorState {
 
 	@Override
 	public MultiPredatorState movePrey(Position action) {
-		// TODO Auto-generated method stub
-		return null;
+		Random random=new Random();
+		double randomNumber=random.nextDouble();
+		if(randomNumber>=randomChanceForWait)
+		{
+			BasicMPState newState=new BasicMPState();
+			for(String name:this.predatorPositions.keySet())
+			{
+				newState.addPredator(name, this.predatorPositions.get(name).copy().subtract(action));
+			}
+			return newState;
+		}	
+		else
+			return this.copy();
 	}
 
 	@Override
 	public MultiPredatorState copy() {
-		// TODO Auto-generated method stub
-		return null;
+		BasicMPState newState=new BasicMPState();
+		for(String name:this.predatorPositions.keySet())
+		{
+			newState.addPredator(name, this.predatorPositions.get(name).copy());
+		}
+		return newState;
+
 	}
 
 	@Override
 	public boolean predatorWins() {
-		// TODO Auto-generated method stub
-		return false;
+		ArrayList<Position> allPosition=new ArrayList<Position>();
+		boolean preyCatched=false;
+		for(Position position: predatorPositions.values())
+		{
+			if(position.getX()==0 && position.getY()==0)
+				preyCatched=true;
+			if(allPosition.contains(position))
+				return false;
+			else
+				allPosition.add(position);
+		}
+		return preyCatched;
 	}
 
 	@Override
 	public MultiPredatorState movePredator(String name, Position action) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		BasicMPState newState=(BasicMPState)this.copy();
+		newState.replacePredator(name, this.predatorPositions.get(name).copy().add(action));	
+		return newState;
+	}
+	
+	/*
+	 * the same behavior as addPredator, only for clear and intuitive name
+	 */
+	public void replacePredator(String name, Position position) {
+		this.predatorPositions.put(name, position);
 	}
 
 	/**
