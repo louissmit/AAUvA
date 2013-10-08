a48157db4a9284d564b2298be70e8bae38b75676
Lex <a.utama91@gmail.com>
2013-10-08 14:31:32 +0200

    QTable implementation

--- AAUvA/src/hunt/model/QTable.java
+++ AAUvA/src/hunt/model/QTable.java
@@ -17,34 +17,38 @@ public class QTable {
 	public void update(HuntState state, HashMap<Position, Double> values) {
 		this.table.put(state, values);
 	}
-	
-	/** Get Q-values for a certain state 
-	 * @return */
-	public HashMap<Position, Double> getQValues(HuntState state) {
-		if (!this.table.containsKey(state)) {
-			this.table.put(state, new HashMap<Position, Double>());
-		}
-		return this.table.get(state);
-	}
-	
-	public Double getQValue(HuntState state, Position action) {
-		return this.table.get(state).get(action);
-	}
-	
 	public void update(HuntState state, Position action, Double value) {
-		if (!this.table.containsKey(state)) {
-			this.table.put(state, new HashMap<Position, Double>());
-		}
+		this.assertStatePresent(state);
 		this.table.get(state).put(action, value);
 	}
-	
-	public Double getQValue(StateActionPair stateActionPair) {
-		// TODO implement
-		return 0.0;
+	public void update(StateActionPair stateActionPair, Double value) {
+		this.update(stateActionPair.getState(), stateActionPair.getAction(), value);
 	}
 	
-	public void update(StateActionPair stateActionPair, Double value) {
-		// TODO implement
+	/** Get Q-values for a certain state */
+	public HashMap<Position, Double> getQValues(HuntState state) {
+		this.assertStatePresent(state);
+		return this.table.get(state);
+	}
+	public Double getQValue(HuntState state, Position action) {
+		this.assertStateActionPresent(state, action);
+		return this.table.get(state).get(action);
+	}
+	public Double getQValue(StateActionPair stateActionPair) {
+		return this.getQValue(stateActionPair.getState(), stateActionPair.getAction());
+	}
+	
+	/** Make sure the requested value is present */
+	protected void assertStatePresent(HuntState state) {
+		if (!this.table.containsKey(state)) {
+			this.table.put(state, new HashMap<Position, Double>());
+		}
+	}
+	protected void assertStateActionPresent(HuntState state, Position action) {
+		this.assertStatePresent(state);
+		if (!this.table.get(state).containsKey(action)) {
+			this.table.get(state).put(action, 0.0);
+		}
 	}
 	
 }
