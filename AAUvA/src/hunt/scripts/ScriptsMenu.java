2521361ac013f9495c6ddd02db25e80d5ffe84bc
Hubert <huber_000@146.50.239.128>
2013-10-18 16:00:34 +0200

    iterations

--- AAUvA/src/hunt/scripts/ScriptsMenu.java
+++ AAUvA/src/hunt/scripts/ScriptsMenu.java
@@ -207,7 +207,7 @@ public class ScriptsMenu {
 				sim.setPrey(new RandomPrey());
 				//sim.setPrey(new SmartPrey(policy, q));
 				sim.setStartState(startState);
-				sim.run(5000);
+				sim.run(3000);
 			}
 		}
 		
